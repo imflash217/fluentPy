75d00c29d18b4ebf02e3849e644838076e247e2d
imflash217 <12234745+imflash217@users.noreply.github.com>
2021-09-26 18:09:02 -0400

    binary search

---
+++ src/necaise_dsa_book/binary_search.py
@@ -0,0 +1,25 @@
+"""Binary Search Implementation"""
+
+
+def binary_search(sequence, target):
+    """Binary Search Algorithm"""
+    ## Step-1: Start with the entire sequence
+    low = 0
+    high = len(sequence)
+
+    ## Step-2: Repeatedly subdivide the sequence
+    while low <= high:
+        ## Step-3: Find the middle index
+        mid = (low + high) // 2
+        ## Step-4: Does the midpoint contain the target?
+        if sequence[mid] == target:
+            return mid
+        elif sequence[mid] > target:
+            ## Step-5: Or does the target precede the midpoint
+            high = mid - 1
+        else:
+            ## Step-6: Or does the target follow the midpoint
+            low = mid + 1
+
+    ## Step-7: Finally, return False if the target is not found
+    return False
